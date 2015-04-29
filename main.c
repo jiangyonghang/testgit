f4eeeb779c578246638e1e7f8bc92c23ff5705d4
gotojyh <gotojyh@gmail.com>
2015-04-29 14:18:31 +0800

    yes

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int main()
+{
+	printf("16:59");
+	printf("hello\n");
+	printf("world\n");
+}
+//1st,modify
+//
+//from here
+//1st,modify
+//2nd,modify
+//
+//again
+//1st,modify
+//2nd,modify
+//
+//fork
+//1st,modify
+//2nd,modify
+//
+//
+//the third
+//1st,modify
+//2nd,modify
